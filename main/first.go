277347607f109fefd96be9dc7d10a68e6e03f1d9
Dias Mashikov <diasmashikovnasa@gmail.com>
2023-04-11 14:05:52 +0600

    Some more self-made functions

--- main/first.go
+++ main/first.go
@@ -3,6 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -11,7 +13,204 @@ import (
 var president = "Tokaev"
 
 func main() {
+	src := "main/random-file.txt" 
+	dst := "main/sharrrup-guys.txt"
+	os.Rename(src, dst)
+}
+
+func writeToFile() {
+	file, err := os.Create("main/random-file.txt")
+
+    if err != nil {
+        return
+    }
+    defer file.Close()
+
+	data := []byte("Sharrrap guyzzz!!!!")
+
+    file.Write(data)
+
+	var idx int64 = int64(len(data))
+
+	data2 := []byte("Sharrrap guyzzz!!!!")
+
+	file.WriteAt(data2, idx)
+}
+
+func readFileWithLines() {
+	lines, err := readLines("main/random-file.txt")
+  	if err != nil {
+    	log.Fatalf("readLines: %s", err)
+  	}
+  	// print file contents
+  	for i, line := range lines {
+    	fmt.Println(i, line)
+  	}
+}
+
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+	  return nil, err
+	}
+	defer file.Close()
+  
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+	  lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+  }
+
+func readWholeFile() {
+	b, err := ioutil.ReadFile("main/random-file.txt")
+
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	str := string(b)
+
+	fmt.Println(str)
+}
+
+func fileExists() {
+	if _, err := os.Stat("main/random-file.txt"); err == nil {
+		fmt.Printf("File exists\n")
+	} else {
+		fmt.Printf("File does not exist\n");
+	}
+}
+
+func switchStatement() {
+	switch a := 1; {
+    case a == 2:
+        fmt.Println("The integer was == 2")
+    case a == 5:
+        fmt.Println("The integer was == 5")
+		fallthrough
+    case a == 3:
+        fmt.Println("The integer was == 3")
+        fallthrough
+    case a == 4:
+        fmt.Println("The integer was == 4")
+    case a == 1:
+        fmt.Println("The integer was == 1")
+    default:
+        fmt.Println("default case")
+    }
+}
+
+func whileLoop() {
+	i := 1 
+	max := 20 
+
+	for i < max {
+		fmt.Println(i)
+		i++
+	}
+}
+
+func ifStatements() {
+	var age = 16
+
+	if (age >= 18) {
+		fmt.Println("You can drink")
+	} else if age >= 16 && age < 18 {
+		fmt.Println("You can drive")
+	} else {
+		fmt.Println("You are a kid bruvvv")
+	}
+}
+
+func rangeLoop() {
+	nums := []int{10, 20, 30, 40, 50, 60}
 	
+	for i, num := range nums {
+		fmt.Printf("Index: %d, Number: %d\n", i, num)
+	}
+}
+
+func basicForLoops() {
+	for x := 0; x < 10; x++ {
+		fmt.Println(x)
+	}
+
+	slice := []int{10, 20, 30, 40, 50}
+
+	for i := 0; i < len(slice); i++ {
+		fmt.Println(slice[i])
+	}
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Lamadre %d", i)
+		fmt.Println()
+		for j := 0; j < 10; j++ {
+			fmt.Printf("Bambino %d ", j)
+		}
+		fmt.Println()
+	}
+}
+
+
+func slicesInGoThree() {
+	a := make([]int, 5)
+	printSliceMake("a", a)
+
+	b := make([]int, 0, 5)
+	printSliceMake("b", b)
+
+	c := b[:2]
+	printSliceMake("c", c)
+
+	d := c[2:5]
+	printSliceMake("d", d)
+}
+
+func printSliceMake(s string, x []int) {
+	fmt.Printf("%s len=%d cap=%d %v\n",
+		s, len(x), cap(x), x)
+}
+
+func slicesInGoTwo() {
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
+
+	// Slice the slice to give it zero length.
+	s = s[:0]
+	printSlice(s)
+
+	// Extend its length.
+	s = s[:4]
+	printSlice(s)
+
+	// Drop its first two values.
+	s = s[2:]
+	printSlice(s)
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
+
+
+func slicesInGoOne() {
+	names := [4]string{
+		"John",
+		"Paul",
+		"George",
+		"Ringo",
+	}
+	fmt.Println(names)
+
+	a := names[0:2]
+	b := names[2:4]
+	fmt.Println(a, b)
+
+	b[0] = "XXX"
+	fmt.Println(a, b)
+	fmt.Println(names)
 }
 
 func arraysInGo() {
